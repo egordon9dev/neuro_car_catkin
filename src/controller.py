40b8b30bd945658bac871510b1c8722855fccf52
Ethan Gordon <egordon9dev@gmail.com>
2019-11-09 14:07:46 -0500

    trained the car to accelerate and turn left. works in gazebo!

--- src/controller.py
+++ src/controller.py
@@ -19,7 +19,6 @@ width = 1280
 height = 720
 angular_vel = 0
 trans_vel = 0
-img_arr = None
 
 # acceleration: translational, rotational
 actions_list = [
@@ -34,172 +33,39 @@ actions_list = [
     [[1], [1]]
 ]
 
-
-def dist(p1, p2):
-    return ((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2) ** 0.5;
-
-
-def find_curbs(contours):
-    if len(contours) < 2:
-        print("Error finding curbs: len less than 2")
-
-    p1 = None
-    p2 = None
-    contour1 = None
-    contour2 = None
-    p1_est = (width / 2, 0)
-    p2_est = (width / 2, 0)
-    for contour in contours:
-        m = cv2.moments(contour)
-        if (m["m00"] == 0):
-            continue
-        cx = m["m10"] / m["m00"]
-        cy = m["m01"] / m["m00"]
-        cur = (cx, cy)
-        if p1 is None or dist(p1, p1_est) > dist(cur, p1_est):
-            contour1 = contour
-        elif p2 is None or dist(p2, p2_est) > dist(cur, p2_est):
-            contour2 = contour
-    return (contour1, contour2)
-
-
-def find_center_point(contour1, contour2):
-    m1 = cv2.moments(contour1)
-    m2 = cv2.moments(contour2)
-    p1 = (m1["m10"] / m1["m00"], m1["m01"] / m1["m00"])
-    p2 = (m2["m10"] / m2["m00"], m2["m01"] / m2["m00"])
-    return ((p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2)
-
-
 def callback(img):
     global angular_vel, trans_vel
-    global img_arr
     try:
         cv_image = bridge.imgmsg_to_cv2(img, "bgr8")
     except CvBridgeError as e:
         print(e)
     edges_img = cv2.Canny(cv_image, 50, 250)
     contours_img, contours, hierarchy = cv2.findContours(edges_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE);
-    # src = np.float32([(0, 180), (1280, 180), (700, 360), (580, 360)])
-    # dst = np.float32([(0,180), (1280, 180), (1280, 360), (0, 360)])
-    # M = cv2.getPerspectiveTransform(src, dst)
-    # Warp the image using OpenCV warpPerspective()
-    # warped = cv2.warpPerspective(cv_image, M, (1280, 720))
-    # track = findCurbs(contours)
-    # curbs = find_curbs(contours)
-    # cv2.drawContours(contours_img, curbs, -1, (0,255,0), 3)
-    #
-    # avg_vec = (1,0)
-    # mid_x = width/2
-    # mid_y = height/2
-    reward = 0
-    car_point = (640, 100)
-    for cont in contours:
-        mom = cv2.moments(cont)
-        if (mom["m00"] == 0):
-            continue
-        center_point = (mom["m10"] / mom["m00"], mom["m01"] / mom["m00"])
-        reward += dist(car_point, center_point) + trans_vel
-
-        # [vx1, vy1, x1, y1] = cv2.fitLine(curbs[0], cv2.DIST_L2,0,0,0.01,0.01)
-
-    img_arr = np.asarray(contours_img).flatten()
 
     cv2.imshow("Image window", contours_img)
     cv2.waitKey(3)
 
-
-log_string = String()
-log_string.data = ""
-min_range = 10000
-min_range_angle = 0
-max_range = 0
-max_range_angle = 0
-
-
-def laser_callback(laser_scan):
-    global min_range, min_range_angle, max_range, max_range_angle
-    global log_string
-    avg_range = 0
-    min_range = 100000
-    min_range_angle = 0
-    max_range = 0
-    max_range_angle = 0
-    if len(laser_scan.ranges) > 0:
-        for i, rng in enumerate(laser_scan.ranges):
-            avg_range += rng
-            current_angle = laser_scan.angle_min + i * laser_scan.angle_increment
-            if rng < min_range:
-                min_range = rng
-                min_range_angle = current_angle
-            if rng > max_range:
-                max_range = rng
-                max_range_angle = current_angle
-        avg_range /= len(laser_scan.ranges)
-    # log_string.data = "closest obstacle --- min " + str(min_range_angle) + " " + str(min_range) + " max " + str(max_range_angle) + " " + str(max_range)
-
-
 sub = rospy.Subscriber('neurocar/camera/image_raw', Image, callback)
-ray_sub = rospy.Subscriber('neurocar/laser/scan', LaserScan, laser_callback)
 pub = rospy.Publisher('neurocar/cmd_vel', Twist, queue_size=10)
-log_pub = rospy.Publisher('neurocar/logger', String, queue_size=10)
 rospy.init_node('controller', anonymous=True)
 rate = rospy.Rate(60)  # 10hz
 neurocar_msg = Twist()
 
-
-def polar_to_rect(r, theta):
-    return r * math.cos(theta), r * math.sin(theta)
-
-
-#
-# def main():
-#     global angular_vel, trans_vel
-#     global log_string
-#     while not rospy.is_shutdown():
-
-
-lastAction = (0.0, 0.0)
-
-
 def move(t, x):
     global angular_vel, trans_vel
 
-    prev_min_range = min_range
-
-    acceleration = [0.0, 0.0]
-    for i in range(len(x)):
-        action = actions_list[i]
-        intensity = x[i]
-        acceleration[0] += intensity * action[0][0]
-        acceleration[1] += intensity * action[1][0]
-    trans_vel += acceleration[0]
-    angular_vel += acceleration[1]
+    trans_vel += x[0]
+    angular_vel += x[1]
 
     # send action
     neurocar_msg.linear.x = trans_vel
     neurocar_msg.angular.z = angular_vel
     pub.publish(neurocar_msg)
 
-    if len(log_string.data) > 0:
-        log_pub.publish(log_string)
-        log_string.data = ""
-
     # do action
     rate.sleep()
 
-    # network reward
-    # min_range_vec = polar_to_rect(min_range, min_range_angle)
-    # max_range_vec = polar_to_rect(max_range, max_range_angle)
-    # delta_vec = (max_range_vec[0] - min_range_vec[0], max_range_vec[1] - min_range_vec[1])
-
-    reward = 0
-    # move away from obstacles
-    reward += min_range - prev_min_range
-    # go fast
-    reward += 10 * abs(neurocar_msg.linear.x)
-
-    return reward
+    return x[0] * 10 + x[1] * 10
 
 
 class InputManager:
@@ -213,8 +79,7 @@ class InputManager:
         self.dimensions = dimensions
 
     def function(self, t):
-        global img_arr
-        return img_arr
+        return np.array([1])
 
 class NeuralNet:
     """
@@ -310,9 +175,9 @@ class NeuralNet:
 # print("Syntax correct.")
 def main():
     global width, height, img_arr
-    input_manager = InputManager(lambda t: img_arr, width * height)
+    input_manager = InputManager(np.array([1]), 1)
     network = NeuralNet(input_manager, move)
-    network.run_network(90)
+    network.run_network(20)
     network.close_simulator()
 
 
