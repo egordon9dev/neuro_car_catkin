f60ed1063c899ba493c9ea76f64d12b152f8c221
Ethan Gordon <egordon9dev@gmail.com>
2020-11-17 16:35:19 -0500

    headless, no gui

--- src/neurocar/scripts/train.py
+++ src/neurocar/scripts/train.py
@@ -138,7 +138,7 @@ def optimize_model():
         param.grad.data.clamp_(-1, 1)
     optimizer.step()
 
-num_episodes = 50
+num_episodes = 10000
 append_log("beginning training...")
 for i_episode in range(num_episodes):
     # Initialize the environment and state
